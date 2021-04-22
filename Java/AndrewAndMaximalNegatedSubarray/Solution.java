595fcaa904269e2ad21796a564a68eded33cab30
ankurpaul <suryashi2013@gmail.com>
2021-04-22 18:06:42 +0000

    Solved Andrew and Maximal Negated Subarray Q2

---
+++ Java/AndrewAndMaximalNegatedSubarray/Solution.java
@@ -0,0 +1,96 @@
+package AndrewAndMaximalNegatedSubarray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Solution {
+    ArrayList<Integer> arrayStore;
+    ArrayList<ArrayList<Integer>> filteredStore;
+    int negativeCounts;
+
+    Solution() {
+        arrayStore = new ArrayList<>();
+        filteredStore = new ArrayList<>();
+        negativeCounts = 99999;
+    }
+
+    // Returns true if number is prime else false.
+    public boolean checkPrime(int num) {
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+        return !flag ? true : false;
+    }
+
+    public void generateSubArrays(int size) {
+        for (int startPoint = 0; startPoint < size; ++startPoint) {
+            for (int grps = startPoint; grps < size; ++grps) {
+                ArrayList<Integer> tempArrStore = new ArrayList<>();
+                for (int c = startPoint; c <= grps; ++c) {
+                    tempArrStore.add(arrayStore.get(c));
+                }
+                // System.out.println();
+                boolean firstCheck = checkPrime(tempArrStore.get(0));
+                boolean lastCheck = checkPrime(tempArrStore.get(tempArrStore.size() - 1));
+                if (firstCheck == true && lastCheck == true) {
+                    analyzeSubArray(tempArrStore);
+                }
+            }
+        }
+    }
+
+    public void analyzeSubArray(ArrayList<Integer> input) {
+        int count = 0;
+        for (int looper : input) {
+            if (looper < 0) // Counting the negative numbers
+                ++count;
+        }
+        if (count < negativeCounts) {
+            negativeCounts = count;
+            filteredStore.clear();
+            filteredStore.add(input);
+        }
+        if (count == negativeCounts) {
+            filteredStore.add(input);
+        }
+        // System.out.println(input.toString() + " COUNT = " + count + " NEGATIVE
+        // COUNTS: " + negativeCounts);
+    }
+
+    public void calculateLargestSubArray() {
+        int largestSize = 0;
+        // Calculates the length of the largest sub array with 0 minimum negative prime
+        // numbers
+        for (ArrayList<Integer> looper : filteredStore)
+            largestSize = looper.size() > largestSize ? looper.size() : largestSize;
+        System.out.println(largestSize);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Solution object = new Solution();
+
+        int num = 0;
+        String str = "";
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        num = Integer.parseInt(bufferedReader.readLine());
+        str = bufferedReader.readLine();
+
+        StringTokenizer st = new StringTokenizer(str);
+        while (st.hasMoreTokens()) {
+            int eachNum = Integer.parseInt(st.nextToken());
+            object.arrayStore.add(eachNum);
+        }
+        // System.out.println(object.arrayStore.toString());
+        object.generateSubArrays(num);
+        // System.out.println("FILTERED: " + object.filteredStore.toString());
+        object.calculateLargestSubArray();
+    }
+}
---
+++ README.md
@@ -0,0 +1,67 @@
+﻿## Limak and palindromes 2 ! ![](logo.png)
+
+Limak likes palindromes and recently he came across a simple problem in which he was given a string and he had to report the total number of unique palindromes of length 3 which can be formed by using the characters of the string. Also, he was supposed to list out in lexicographical order all the palindromes of length 3 with each palindrome in a separate line.
+
+**Input Format**
+
+The first line of input contains the length of the string and it is followed by a string of the above mentioned length. **Constraints**
+
+1 <= length of string <= 100000
+
+The string consists of lowercase alphabets
+
+**Output Format**
+
+The first line of input should be an integer which is the total number of unique palindromes of length 3 which can be formed by using the characters in the string. The following lines consist of all unique palindromes of length 3 which are arranged in lexicographical order and can be formed using the characters in the string.
+
+**Sample Input 0**\
+4\
+aabc\
+![](input-one.png)
+
+**Sample Output 0**\
+2\
+aba\
+aca\
+![](output-one.png)
+
+**Explanation 0**
+In this sample test case, we can form two palindromes of length 3 by using the characters in the string. When printed in lexicographical order, aba and aca is printed.
+
+       =================================================================================
+
+## Andrew and Maximal Negated Subarray ![](logo.png)
+
+Andrew likes solving problems and dealing with prime numbers. He recently came across a problem in which he was given a list of numbers and he was supposed to find out a subarray from this list which starts and ends in a prime number and which has minimum count of negative numbers in it. In case two or more such subarrays have the same minimum count of negative numbers then please report an array which has the maximum length from the two.
+
+**Note: The length of the subarray is inclusive of both the starting and ending prime numbers. Input Format**
+
+The first line of input contains an integer n which is the total elements in the list. The following line contains space separated n integers.
+
+**Constraints**
+
+1 <= N <= 10000
+
+-10000 <= element <= 1000000
+
+**Output Format**
+
+Print a single integer which is the length of the subarray which starts and ends with a prime number and has the least count of negative numbers. In case two or more subarrays have the same count of negative numbers, then print the length of the bigger array.
+
+**Sample Input 0**
+
+6
+
+1 2 -3 4 5 7
+
+![](input-two.png)
+
+**Sample Output 0**
+
+2
+
+![](output-two.png)
+
+**Explanation 0**
+
+In this sample test case, the subset between 2 and 5 has one negative number whereas the subset between 5 and 7 has no negative number hence the length of subset from 5 and 7 with both of them as inclusive is 2.
